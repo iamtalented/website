020af1a590b0b9edb8255e518f36e9f5bc723a88
laurenceputra <laurenceputra@gmail.com>
2015-02-02 22:45:48 +0800

    added new logo

--- controllers/2015/index.js
+++ controllers/2015/index.js
@@ -1,6 +1,10 @@
 'use strict';
 
 module.exports = function (router) {
+	router.get('/', function (req, res) {
+		res.render('2015/index');
+	});
+
     router.get('/signup', function (req, res) {
         res.render('2015/signup');
     });
